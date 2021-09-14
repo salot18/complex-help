bb0199fa243ac9ccab56e002cf11201a9e7a09b7
tolas18 <geo18tol@gmail.com>
2021-09-14 13:01:10 +0300

    Add toFixed(4)

--- main.js
+++ main.js
@@ -17,10 +17,10 @@ for (let inputAddField of inputAddFields) {
     let real2 = parseFloat(inputAddFields[2].value);
     let imag2 = parseFloat(inputAddFields[3].value);
     if (!isNaN(real1 + real2)) {
-      sumFields[0].value = real1 + real2;
+      sumFields[0].value = (real1 + real2).toFixed(4);
     }
     if (!isNaN(imag1 + imag2)) {
-      sumFields[1].value = imag1 + imag2;
+      sumFields[1].value = (imag1 + imag2).toFixed(4);
     }
   })
 }
@@ -32,10 +32,10 @@ for (let inputSubField of inputSubFields) {
     let real2 = parseFloat(inputSubFields[2].value);
     let imag2 = parseFloat(inputSubFields[3].value);
     if (!isNaN(real1 - real2)) {
-      diffFields[0].value = real1 - real2;
+      diffFields[0].value = (real1 - real2).toFixed(4);
     }
     if (!isNaN(imag1 - imag2)) {
-      diffFields[1].value = imag1 - imag2;
+      diffFields[1].value = (imag1 - imag2).toFixed(4);
     }
   })
 }
@@ -47,10 +47,10 @@ for (let inputMulField of inputMulFields) {
     let real2 = parseFloat(inputMulFields[2].value);
     let imag2 = parseFloat(inputMulFields[3].value);
     if (!isNaN(real1 * real2 - imag1 * imag2)) {
-      prodFields[0].value = real1 * real2 - imag1 * imag2;
+      prodFields[0].value = (real1 * real2 - imag1 * imag2).toFixed(4);
     }
     if (!isNaN(real1 * imag2 + imag1 * real2)) {
-      prodFields[1].value = real1 * imag2 + imag1 * real2;
+      prodFields[1].value = (real1 * imag2 + imag1 * real2).toFixed(4);
     }
   })
 }
@@ -62,10 +62,10 @@ for (let inputDivField of inputDivFields) {
     let real2 = parseFloat(inputDivFields[2].value);
     let imag2 = parseFloat(inputDivFields[3].value);
     if (!isNaN((real1 * real2 + imag1 * imag2) / (real2 * real2 + imag2 * imag2))) {
-      quotFields[0].value = (real1 * real2 + imag1 * imag2) / (real2 * real2 + imag2 * imag2);
+      quotFields[0].value = ((real1 * real2 + imag1 * imag2) / (real2 * real2 + imag2 * imag2)).toFixed(4);
     }
     if (!isNaN((imag1 * real2 - real1 * imag2) / (real2 * real2 + imag2 * imag2))) {
-      quotFields[1].value = (imag1 * real2 - real1 * imag2) / (real2 * real2 + imag2 * imag2);
+      quotFields[1].value = ((imag1 * real2 - real1 * imag2) / (real2 * real2 + imag2 * imag2)).toFixed(4);
     }
   })
 }
@@ -82,10 +82,10 @@ for (let polarInputField of polarInputFields) {
     let magnitude = parseFloat(polarInputFields[0].value);
     let angle = parseFloat(polarInputFields[1].value);
     if (!isNaN(magnitude * Math.cos(degToRad(angle)))) {
-      cartesianTransFields[0].value = magnitude * Math.cos(degToRad(angle));
+      cartesianTransFields[0].value = (magnitude * Math.cos(degToRad(angle))).toFixed(4);
     }
     if (!isNaN(magnitude * Math.sin(degToRad(angle)))) {
-      cartesianTransFields[1].value = magnitude * Math.sin(degToRad(angle));
+      cartesianTransFields[1].value = (magnitude * Math.sin(degToRad(angle))).toFixed(4);
     }
   })
 }
@@ -95,10 +95,10 @@ for (let cartesianInputField of cartesianInputFields) {
     let real = parseFloat(cartesianInputFields[0].value);
     let imag = parseFloat(cartesianInputFields[1].value);
     if (!isNaN(Math.sqrt(real * real + imag * imag))) {
-      polarTransFields[0].value = Math.sqrt(real * real + imag * imag);
+      polarTransFields[0].value = (Math.sqrt(real * real + imag * imag)).toFixed(4);
     }
     if (!isNaN(radToDeg(Math.atan2(real, imag)))) {
-      polarTransFields[1].value = radToDeg(Math.atan2(imag, real));
+      polarTransFields[1].value = (radToDeg(Math.atan2(imag, real))).toFixed(4);
     }
   })
 }
